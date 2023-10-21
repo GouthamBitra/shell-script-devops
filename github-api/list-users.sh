a60a573f830922600ce4159d252f745cf4449fd4
GouthamBitra <goutham.yb@gmail.com>
2023-10-21 16:43:23 +0100

    Test1

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -3,25 +3,23 @@
 ####################################
 # Author: Goutham Bitra
 # Date: 21-10-2023
-# About: This script will list the users of github repository
-# Input: We have to export username and token Eg: export username="GouthamBitra",
-# export token="ghp_*****"
-# We should pass two arguments while executing the script eg: ./list-users.sh "REPO_OWNER" "REPO_NAME"
+# About: This script lists the users with read access to a GitHub repository.
+# Input: You need to export your GitHub username and token.
+#        Example: export username="GouthamBitra", export token="ghp_*****"
+#        You should pass two arguments while executing the script, e.g.: ./list-users.sh "REPO_OWNER" "REPO_NAME"
 #
 ######################################
 
-helper()
-
 # GitHub API URL
-API_URL = "https://api.github.com"
+API_URL="https://api.github.com"
 
 # GitHub username and personal access token
-USERNAME=$username
-TOKEN=$token
+USERNAME="$username"
+TOKEN="$token"
 
 # User and Repository information
-REPO_OWNER=$1
-REPO_NAME=$2
+REPO_OWNER="$1"
+REPO_NAME="$2"
 
 # Function to make a GET request to the GitHub API
 github_api_get() {
@@ -32,13 +30,12 @@ github_api_get() {
   curl -s -u "${USERNAME}:${TOKEN}" "${url}"
 }
 
-# Function to list users with read access to repository
-
+# Function to list users with read access to the repository
 list_users_with_read_access() {
-  local endpoint = "repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+  local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
   # Fetch the list of collaborators on the repository
-  collaborators = "$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+  collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
 
   # Display the list of collaborators with read access
   if [[ -z "$collaborators" ]]; then
@@ -46,19 +43,21 @@ list_users_with_read_access() {
   else
     echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
     echo "$collaborators"
-  fi    
+  fi
 }
 
-# This will check if the user is executing the shell properly or not
-
+# Function to provide usage instructions
 helper() {
   expected_cmd_args=2
-  if [ $# -ne $expected_cmd_args]; then
+  if [ $# -ne $expected_cmd_args ]; then
     echo "Usage: ./list-users.sh <REPO_OWNER> <REPO_NAME>"
-  fi  
+    exit 1
+  fi
 }
 
-# Main script
+# Check the number of command-line arguments
+helper "$@"
 
+# Main script
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
